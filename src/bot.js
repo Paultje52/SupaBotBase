e053d163c5ec6a295c2ca52005b590ce3ededf3a
Paultje52 <Paultje52@gmail.com>
2020-12-29 23:02:58 +0100

    Updated "example" code and added config file

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,5 @@
 const BaseBot = require("./util/BaseBot");
+const { MessageEmbed } = require("discord.js");
 
 (async () => {
   console.log(`\x1b[32m\x1b[1mLoading bot Bot...\x1b[0m`);
@@ -9,6 +10,19 @@ const BaseBot = require("./util/BaseBot");
   });
   // Load everything
   await bot.loadAll();
+  // Set config
+  bot.setConfig("config.js");
+  // Set database
+  bot.createDatabase("database.sqlite");
+  // Add message function
+  bot.addMessageFunction("embed", () => {
+    return new MessageEmbed().setFooter("©BaseBot").setColor("#2f3136");
+  });
+  // Add message handler
+  bot.addMessageHandler((message) => {
+    console.log(1);
+    // Return false > Stop message handling.
+  });
   // Start the bot
   console.log("Starting bot...");
   await bot.start();
---
+++ src/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  disable: {
+    message: false,
+    slash: false,
+    mentionPrefix: false
+  },
+  prefix: ".",
+
+  database: {
+    defaultGuildSettings: {
+      prefix: "."
+    },
+    defaultUserSettings: {}
+  }
+};
