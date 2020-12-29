8a91ee14015806b0d91f2bf8daab73b9adad882c
Paultje52 <Paultje52@gmail.com>
2020-12-29 22:54:10 +0100

    Added message handler

---
+++ src/util/MessageHandler.js
@@ -0,0 +1,64 @@
+module.exports = class MessageHandler {
+
+  getEvent() {
+    return "message";
+  }
+
+  onExecute(message) {
+    if (this.main.config.disable && this.main.config.disable.message) return;
+
+    if (message.author.bot) return;
+
+    if (this.main.database && this.main.config.database) this.loadSettings(message);
+    let prefix = this.getFullPrefix(message);
+    if (!message.content.toLowerCase().startsWith(prefix)) return;
+
+    let {args, command} = this.getMessageAndArgs(message.content, prefix);
+    let cmdFile = this.getCommand(command);
+    if (!cmdFile) return;
+
+    // TODO: Check security
+
+    this.addSlashCommandSupportData(message);
+    cmdFile.onExecute(message, args);
+  }
+
+  addSlashCommandSupportData(message) {
+    message.isSlashCommand = false;
+    message.answerCommand = (content) => {
+      message.channel.send(content);
+    }
+  }
+
+  getCommand(command) {
+    return this.main.commands.get(command) || this.main.aliases.get(command);
+  }
+
+  getMessageAndArgs(content, prefix) {
+    let args = content.slice(prefix.length).trim().split(/ +/g);
+    let command = args.shift().toLowerCase();
+    return {args, command};
+  }
+
+  loadSettings(message) {
+    message.guild.settings = this.main.database.get(`guild-${message.guild.id}`);
+    if (!message.guild.settings) message.guild.settings = this.main.config.database.defaultGuildSettings || {};
+
+    message.author.settings = this.main.database.get(`user-${message.author.id}`);
+    if (!message.author.settings) message.author.settings = this.main.config.database.defaultUserSettings || {};
+  }
+
+  getFullPrefix(message) {
+    if (this.main.config.disable && this.main.config.disable.mentionPrefix) return this.getPrefix(message);
+    let prefixMention = new RegExp(`^<@!?${this.client.user.id}> `);
+    let match = message.content.match(prefixMention);
+    return match ? match[0] : this.getPrefix(message);
+  }
+
+  getPrefix(message) {
+    if (this.main.database && message.guild.settings.prefix) return message.guild.settings.prefix;
+    if (this.main.config.prefix) return this.main.config.prefix;
+    return ".";
+  }
+
+}
